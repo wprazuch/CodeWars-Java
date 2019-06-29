7858528b229e617883a4473af745d608cd6173f3
WojPrauch <wojciechprazuch3@gmail.com>
2019-06-29 23:07:13 +0200

    Update

--- src/main/java/Accumul.java
+++ src/main/java/Accumul.java
@@ -1,11 +1,19 @@
-public class Mumbling {
+public class Accumul {
 
     public static String accum(String s) {
         s = s.toLowerCase();
         String[] splitted = s.split("");
-
-        return null;
-
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i < splitted.length; i++)
+        {
+            output.append(splitted[i].toUpperCase());
+            for(int j = 0; j < i; j++)
+            {
+                output.append(splitted[i]);
+            }
+            output.append("-");
+        }
+        return output.toString().substring(0, output.toString().length()-1);
     }
 
 }
--- src/test/java/AccumulTest.java
+++ src/test/java/AccumulTest.java
@@ -1,20 +1,19 @@
+import static org.junit.Assert.*;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
-public class MumblingTest {
 
-    @Test
-    public void testExample() {
-        int[] exampleTest1 = {2,6,8,-10,3};
-        int[] exampleTest2 = {206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781};
-        int[] exampleTest3 = {Integer.MAX_VALUE, 0, 1};
-        assertEquals(3, FindOutlier.find(exampleTest1));
-        assertEquals(206847684, FindOutlier.find(exampleTest2));
-        assertEquals(0, FindOutlier.find(exampleTest3));
+public class AccumulTest {
 
-        assertEquals(6, FindOutlier.find(new int[]{1, 3, 5, 7, 6, 3, 9, 13}));
-        assertEquals(131, FindOutlier.find(new int[]{32, 68, 92, 102, 131, 144}));
-        assertEquals(-3, FindOutlier.find(new int[]{0, 2, 4, 6, -3}));
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
     }
-
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests accum");
+        testing(Accumul.accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        testing(Accumul.accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        testing(Accumul.accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        testing(Accumul.accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        testing(Accumul.accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
 }
