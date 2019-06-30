9237dcab622463a232b688f82f6a6a0ff4deaf7b
WojPrauch <wojciechprazuch3@gmail.com>
2019-06-30 10:05:37 +0200

    Shortest Word

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,8 +1,19 @@
 //https://www.codewars.com/kata/get-the-middle-character/train/java
+//https://www.codewars.com/kata/shortest-word/train/java
+
+import java.util.stream.Stream;
 
 class Kata {
     public static String getMiddle(String word) {
         int length = word.length();
         return length % 2 == 0 ? word.substring(length/2-1, length/2+1) : String.valueOf(word.charAt(length/2));
     }
+
+    public static int findShort(String s) {
+        return Stream.of(s.split(" "))
+                .mapToInt(word -> word.length())
+                .min()
+                .getAsInt();
+    }
+
 }
---
+++ src/test/java/KataTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class KataTest {
+    @Test
+    public void findShort() throws Exception {
+        assertEquals(3, Kata.findShort("bitcoin take over the world maybe who knows perhaps"));
+        assertEquals(3, Kata.findShort("turns out random test cases are easier than writing out basic ones"));
+    }
+
+}
